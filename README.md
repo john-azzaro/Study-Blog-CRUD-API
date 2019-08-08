0dd65bf3d0d72fd52e7e1f429413ce0bc107e5b4
John Azzaro <jazzaro@temple.edu>
2019-08-07 18:22:44 -0700

    updated server.js, redme

--- README.md
+++ README.md
@@ -3,6 +3,15 @@
 <br>
 
 ## What is CRUD API Study?
-The "CRUD API Study" explores the basic functionality of the CRUD operations using a simple blog as an example.
+The "CRUD API Study" explores the basic functionality of Create, Read, Update, and Delete (i.e. CRUD) operations which
+are the four basic functions of persistent storage.  This study uses a simple blog application as an example.
 
 <br>
+
+In an express applicaion such as this study, CRUD operations will help CREATE a blog post, READ the blog post so that the user
+can see it, UPDATE the blog post, and DELETE the blog post if necessary.
+
+<br>
+
+## Does CRUD API Study feature commentary?
+Yes!  
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require("express");                        // include express
+const morgan = require("morgan");                          // include morgan
 
-const blogPostsRouter = require("./blogPostsRouter");
-const app = express();
+const blogPostsRouter = require("./blogPostsRouter");      // use blogPosts router
+const app = express();                                     // instantiate the app
 
-app.use(morgan("common"));
-app.use(express.json());
+app.use(morgan("common"));                                // use morgan middleware with common layout
+app.use(express.json());                                  // use express.json
 
 app.use("/blog-posts", blogPostsRouter);                 // you need to import `blogPostsRouter` router and route requests to HTTP requests to `/blog-posts` to `blogPostsRouter`
 
-app.listen(process.env.PORT || 8080, () => {
+app.listen(process.env.PORT || 8080, () => {                                     // listen on port 8080 for requests.
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
